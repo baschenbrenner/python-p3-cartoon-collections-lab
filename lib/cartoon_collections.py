b0a1873e925570abe0c18a64041eeb2e89265597
baschenbrenner <baschenbrenner@hotmail.com>
2022-11-04 11:36:13 -0500

    solved it my own way - hope it's good

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,28 @@
-def roll_call_dwarves():
+def roll_call_dwarves(list):
+    for index, dwarf in enumerate(list):
+        print(f'{index+1}. {dwarf}')
     pass
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(arr):
+    result = map(change,arr)
+    return  list(result)
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(arr):
+    result = map(check_length, arr)
+    return any(list(result))
+    
 
-def find_the_cheese():
-    pass
+def find_the_cheese(arr):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for el in arr:
+        if el in cheeses:
+            return el
+    return None
+    
+
+def change(str):
+    x= str[0].capitalize()
+    return x + str[1:len(str)] + "!"
+
+def check_length(x):
+    return len(x) > 4
